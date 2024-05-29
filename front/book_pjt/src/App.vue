2a7d84759b719fc9ab21d37a06aaf4b748ff2222
Subiniiie <subin57419@gmail.com>
2024-05-29 21:45:42 +0900

    네비바 지정

--- front/book_pjt/src/App.vue
+++ front/book_pjt/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <header>
+    <nav>
+      <RouterLink :to="{ name: 'new-book' }">최신도서</RouterLink>
+      <RouterLink :to="{ name: 'bestseller' }">베스트셀러</RouterLink>
+      <RouterLink :to="{ name: 'category'}">장르</RouterLink>
+      <RouterLink :to="{ name: 'home'}">HOME</RouterLink>
+      <RouterLink :to="{ name: 'author'}">인기작가</RouterLink>
+      <RouterLink :to="{ name: 'community'}">커뮤니티</RouterLink>
+      <RouterLink :to="{ name: 'mypage', params: {'id': 1}}">마이페이지</RouterLink>
+      <RouterLink :to="{ name: 'login'}">로그인</RouterLink>
+      <RouterLink :to="{ name: 'signup'}">회원가입</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
