dc787fb63aaf06cb5c76bea75129078869c6efca
Subiniiie <subin57419@gmail.com>
2024-06-08 13:57:32 +0900

    베스트셀러 출력

--- front/book_pjt/src/stores/book.js
+++ front/book_pjt/src/stores/book.js
@@ -4,12 +4,13 @@ import axios from 'axios'
 
 export const useBookStore = defineStore('book', () => {
   const newBooks = ref([])
+  const bestseller = ref([])
   const API_KEY = import.meta.env.VITE_APP_ALADIN_API_KEY
 
   const getNewbooks = function() {
     axios({
       method: 'get',
-      url: `https://cors-anywhere.herokuapp.com/http://www.aladin.co.kr/ttb/api/ItemList.aspx?`,
+      url: 'https://cors-anywhere.herokuapp.com/http://www.aladin.co.kr/ttb/api/ItemList.aspx?',
       params: {
           ttbkey: API_KEY,
           QueryType: 'ItemNewAll',
@@ -27,10 +28,32 @@ export const useBookStore = defineStore('book', () => {
         console.log('에러는',error)
       })
     }
+
+    const getBestseller = function() {
+      axios({
+        method: 'get',
+        url: 'https://cors-anywhere.herokuapp.com/http://www.aladin.co.kr/ttb/api/ItemList.aspx?',
+        params: {
+          ttbkey: API_KEY,
+          QueryType: 'Bestseller',
+          MaxResults: 50,
+          start: 1,
+          SearchTarget: 'Book',
+          output: 'js',
+          Version: '20131101'
+        }
+      })
+        .then((response) => {
+          bestseller.value = response.data.item
+        })
+        .catch((error) => {
+          console.log('에러는', error)
+        })
+    }
     
 
   return {
-    newBooks,
-    getNewbooks
+    newBooks, bestseller,
+    getNewbooks, getBestseller
   }
 })
--- front/book_pjt/src/views/BestsellerView.vue
+++ front/book_pjt/src/views/BestsellerView.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <h1>베스트샐러</h1>
+    <ul>
+      <div
+        v-for="bestseller in BookStore.bestseller"
+        :key="bestseller.title"
+        class="bestseller"
+      >
+      <img :src="bestseller.cover" alt="{{ bestseller.cover }}" @click="makemodal(bestseller)">
+      </div>
+    </ul>
+  </div>
+  <div v-if="isActive" id="modal">
+    <img :src="selectedBook.cover" alt="{{ selected.cover }}">
+    <h1>{{ selectedBook.title }}</h1>
+    <h2>{{ selectedBook.author }}</h2>
+    <h4>{{ selectedBook.publisher }}</h4>
+    <h4>{{ selectedBook.pubDate }}</h4>
+    <h4>{{ selectedBook.priceSales }}</h4>
+    <p>{{ selectedBook.description }}</p>
   </div>
 </template>
 
@@ -10,12 +28,29 @@ import { useBookStore } from '@/stores/book'
 
 const BookStore = useBookStore()
 
-// onMounted(() => {
-//   BookStore.getBestseller()
-// })
+onMounted(() => {
+  BookStore.getBestseller()
+})
 
+// 이미지 클릭하면 책 정보
+const isActive = ref(false)
+const selectedBook = ref(null)
+
+const makemodal = function(bestseller) {
+  selectedBook.value = bestseller
+  isActive.value = true
+}
 </script>
 
 <style scoped>
+.bestseller {
+    border: 1px solid black;
+  }
+
+#modal {
+  width: 1800px;
+  height: 1000px;
+  border: 5px solid black;
+}
 
 </style>
